f02b3ff6580de92d199be4bbce3a9cfdb79b6a4e
irna vahoaka <vahoaka@debian.debian>
2023-04-12 02:01:36 -0400

    Back end and Front end setup are finished / starting project

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import colors from 'colors';
+
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:5173/' }));
+app.use(express.json());
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/uploads')));
+
+app.get('/', (req, res) => {
+  res.send('Api is running...');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.green
+      .underline.bold
+  )
+);
